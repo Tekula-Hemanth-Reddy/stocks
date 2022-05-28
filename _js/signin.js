fe4098643bec9288138ace619066193a6dabb243
Hemanth Reddy.T <62586759+Tekula-Hemanth-Reddy@users.noreply.github.com>
2022-05-28 17:40:52 +0530

    intial commit

---
+++ _js/signin.js
@@ -0,0 +1,43 @@
+var clicked = 0;
+
+  $(".toggle-password").click(function (e) {
+     e.preventDefault();
+
+    $(this).toggleClass("toggle-password");
+      if (clicked == 0) {
+        $(this).html('<span class="material-icons">visibility_off</span >');
+         clicked = 1;
+      } else {
+         $(this).html('<span class="material-icons">visibility</span >');
+          clicked = 0;
+       }
+
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+       input.attr("type", "text");
+    } else {
+       input.attr("type", "password");
+    }
+});
+
+var pressed = 0;
+
+  $(".toggle-con-password").click(function (e) {
+     e.preventDefault();
+
+    $(this).toggleClass("toggle-con-password");
+      if (pressed == 0) {
+        $(this).html('<span class="material-icons">visibility_off</span >');
+        pressed = 1;
+      } else {
+         $(this).html('<span class="material-icons">visibility</span >');
+         pressed = 0;
+       }
+
+    var input = $($(this).attr("toggle"));
+    if (input.attr("type") == "password") {
+       input.attr("type", "text");
+    } else {
+       input.attr("type", "password");
+    }
+});
